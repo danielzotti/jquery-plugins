8835ed1da51864f88c51dbba9ef0a356b73b2907
Daniel Zotti <daniel.zotti87@gmail.com>
2018-08-14 13:57:11 +0200

    Aggiornamento internalMenuAffix

--- plugins/jquery.internalMenuAffix.js
+++ plugins/jquery.internalMenuAffix.js
@@ -3,7 +3,8 @@
     $.fn.internalMenuAffix = function (options) {
         // This is the easiest way to have default options.
         var settings = $.extend({
-            containerClass: null,
+            containerSelector: null,
+            //decide da quale larghezza di finestra il plugin è attivo
             fromWidth: 0,
             //decide dove posizionarsi quando è affix
             positionTopFunction: function () {
@@ -27,7 +28,7 @@
             bottomElementPosition: 0
         }, options);
 
-        var selector = this;
+        var element = this;
 
         var offsetSettings = {
             top: function () {
@@ -35,46 +36,46 @@
             }
         };
         if (isActive(settings)) {
-            selector.affix({
+            element.affix({
                 offset: offsetSettings
             });
         }
 
-        selector.on("affixed-top.bs.affix", function () {
+        element.on("affixed-top.bs.affix", function () {
             if (isActive(settings)) {
-                selector.css(getAffixTopCss(selector, settings));
+                element.css(getAffixTopCss(element, settings));
             }
         });
 
-        selector.on("affixed.bs.affix", function () {
+        element.on("affixed.bs.affix", function () {
             if (isActive(settings)) {
-                selector.css(getAffixCss(selector, settings));
+                element.css(getAffixCss(element, settings));
             }
         });
 
         $(window).on("load resize scroll", function () {
             if (isActive(settings)) {
-                selector.affix({
+                element.affix({
                     offset: offsetSettings
                 });
-                //selector.affix('checkPosition');
+                //element.affix('checkPosition');
 
-                if (selector.hasClass('affix-top')) {
-                    selector.css(getAffixTopCss(selector, settings));
+                if (element.hasClass('affix-top')) {
+                    element.css(getAffixTopCss(element, settings));
                 }
 
-                if (selector.hasClass('affix')) {
-                    selector.css(getAffixCss(selector, settings));
+                if (element.hasClass('affix')) {
+                    element.css(getAffixCss(element, settings));
                 }
             } else {
-                selector.css(getResettedCss());
+                element.css(getResettedCss());
             }
 
         })
         return this;
     }
 
-    function getAffixTopCss(selector, settings) {
+    function getAffixTopCss(element, settings) {
         return {
             top: 'auto',
             width: 'auto',
@@ -84,11 +85,11 @@
         }
     }
 
-    function getAffixCss(selector, settings) {
+    function getAffixCss(element, settings) {
         return {
             top: settings.positionTopFunction(),
-            width: getMenuWidth(selector, settings),
-            height: getMenuHeight(selector, settings),
+            width: getMenuWidth(element, settings),
+            height: getMenuHeight(element, settings),
             position: 'fixed',
             overflow: 'auto'
         }
@@ -104,22 +105,22 @@
         }
     }
 
-    function getMenuWidth(selector, settings) {
-        return (settings.containerClass != null ? $(settings.containerClass).outerWidth() : selector.outerWidth())
+    function getMenuWidth(element, settings) {
+        return (settings.containerSelector != null ? $(settings.containerSelector).outerWidth() : element.parent().outerWidth())
     }
 
-    function getMenuHeight(selector, settings) {
-        if (isFooterInWindow(selector, settings)) {
+    function getMenuHeight(element, settings) {
+        if (isFooterInWindow(element, settings)) {
             return ($(document).height() - $(document).scrollTop() - settings.positionTopFunction() - settings.bottomElementHeightFunction())
         }
         return ($(window).height() - settings.positionTopFunction());
     }
 
-    function getMenuRealHeight(selector) {
-        return selector.outerHeight(true);
+    function getMenuRealHeight(element) {
+        return element.outerHeight(true);
     }
 
-    function isFooterInWindow(selector, settings) {
+    function isFooterInWindow(element, settings) {
         return ($(document).scrollTop() + $(window).height() > settings.bottomElementPositionFunction());
     }
 
@@ -128,3 +129,30 @@
     }
 
 }(jQuery));
+
+
+// $('#internal-menu-content').internalMenuAffix({
+//     containerSelector: '.internal-menu-container',
+//     //decide da quale larghezza di finestra il plugin è attivo
+//     fromWidth: 768,
+//     //decide dove posizionarsi quando è affix
+//     positionTopFunction: function () {
+//         return $('.navbar').outerHeight(true);
+//     },
+//     //decide quando eseguire l'affix
+//     triggerTopFunction: function () {
+//         return $('.esfri-menu').outerHeight(true) + $('.section-header').outerHeight(true); // - 10
+//     },
+//     //adatta l'altezza del menu in base a quando arriva l'elemento sotto (solitamente footer)
+//     bottomElementHeightFunction: function () {
+//         return $('.footer').outerHeight(true);
+//     },
+//     //adatta l'altezza del menu in base alla dimensione della finestra
+//     bottomElementPositionFunction: function () {
+//         return $('.footer').position().top;
+//     },
+// });
+// $("#internal-menu-content").mCustomScrollbar({
+//     theme: "dark",
+//     //autoHideScrollbar: true
+// });
