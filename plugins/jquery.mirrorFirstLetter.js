51b842a9aeb51dc9750fefacb3c3a888ca661a1f
Daniel Zotti <daniel.zotti87@gmail.com>
2018-08-23 18:00:23 +0200

    aggiunto plugin mirror first letter

---
+++ plugins/jquery.mirrorFirstLetter.js
@@ -0,0 +1,26 @@
+﻿(function ($) {
+
+    $.fn.mirrorFirstLetter = function (options) {
+
+        var settings = $.extend({
+            class: 'first-letter',
+        }, options);
+
+        var selector = this;
+        selector.each(function () {
+            var $container = $(this);
+            var text = $container.text();
+            var firstLetter = null;
+            if (text && text.length > 0) {
+                text = $.trim(text);
+            }
+            if (text.length > 0) {
+                firstLetter = text.charAt(0);
+                $container.prepend("<div class='" + settings.class + "'>" + firstLetter + "</div>");
+            }
+        })
+
+        return this;
+    }
+
+}(jQuery));
