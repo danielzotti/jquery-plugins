81a3e9fb0fdecdc9c055842b445016da3cf6a284
Daniel Zotti <daniel.zotti87@gmail.com>
2018-08-24 14:15:31 +0200

    Aggiunto slickFixHeight

---
+++ plugins/jquery.slickFixHeight.js
@@ -0,0 +1,59 @@
+// It works only with Slick Slider
+(function ($) {
+
+    var $slick;
+    var $track;
+    var $slides;
+
+    $.fn.slickFixHeight = function (options) {
+
+        var settings = $.extend({
+            trackClass: '.slick-track', //The parent of the list of slides
+        }, options);
+
+        // init variables
+
+        $slick = this;
+        $track = $slick.find(settings.trackClass);
+        $slides = $track.children();
+
+        //on load
+        equalizeHeights();
+
+        //on resize
+        $(window).on("resize", throttle(equalizeHeights, 200))
+
+        return this;
+    }
+
+    equalizeHeights = function () {
+        $slides.css('min-height', '');
+        $slides.css('height', '');
+        var minSlideHeight = $track.height();
+        setTimeout(function () {
+            $slides.css('min-height', minSlideHeight);
+            $slides.css('height', minSlideHeight);
+        }, 0)
+    }
+
+    throttle = function (fn, frequency) {
+        frequency = frequency || 100;
+        var timeout = false;
+
+        return function () {
+            if (timeout) {
+                return;
+            }
+
+            timeout = setTimeout(function () {
+                timeout = false;
+            }, frequency);
+
+            //Execute function
+            if (fn) {
+                fn();
+            }
+        };
+    }
+
+}(jQuery));
